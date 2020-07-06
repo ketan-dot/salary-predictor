2183e0635686ad72035270f2f0e9f33e3fa5fcef
ketan-dot <67870430+ketan-dot@users.noreply.github.com>
2020-07-06 20:20:30 +0530

    Add files via upload
    uploaded files for first time

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__) #Initialize the flask App
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    """ 
+    3 values = request.form.values()
+    for x in 3 values:
+        int(x)
+        int_features.append(x)
+        
+    int_f=[4,66,73]
+    """
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
